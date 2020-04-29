0e0c121c3924803bd6b0ffc4cdf3fcd49a45e674
AndersonTeala <andersonteala@gmail.com>
2020-04-29 04:25:27 -0300

    Resolvido erro, retirado as % das linhas INSS.

--- Aula02_Ex1_Projeto1.c
+++ Aula02_Ex1_Projeto1.c
@@ -4,16 +4,22 @@ int main() {
     printf("Digite seu salario bruto");
     scanf("%f", &salario);
     
-    if(salario <= 1693.72)
-    {
+    if(salario <= 1693.72){
         inss =salario * 0.08;
     }
     else
-    if(salario >= 1693.73 && salario <= 2822.90)
-    {
+    if(salario >= 1693.73 && salario <= 2822.90){
         inss = salario * 0.09;
     }
-    
+
+    if(salario <= 2822.91){
+        inss = salario * 0.11;
+    }
+    else
+    if(salario >= 2822.92 && salario <= 5646.80){
+        inss = salario * 0.11;
+    }
+
     sal_liquido = (salario - inss) -ir;
     
     printf ("\n Desconto Inss: %f\n", inss);
