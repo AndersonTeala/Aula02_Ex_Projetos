0d484c47ccae943e0c99ca1e5b1cba44990ac639
AndersonTeala <andersonteala@gmail.com>
2020-04-29 02:07:06 -0300

    Aula02 Projeto1

---
+++ Aula02_Ex2_Projeto2.c
@@ -0,0 +1,29 @@
+int main() {
+    int i;
+    
+    do{
+        printf("\n Digite um numero do sabor \n");
+        
+        printf("\t (1) ...flocos \n");
+        printf("\t (2) ...morango \n");
+        printf("\t (3) ...leite condensado \n");
+        
+        scanf("%d", &i);
+        
+    }while((i<1) || (i>3));
+    
+    switch(i){
+        case 1:
+            printf("\t\t Voce escolheu flocos");
+            break;
+            
+        case 2:
+            printf("\t\t Voce escolheu morango");
+            break;
+        case 3:
+            printf("\t\t Voce escolheu leite condensado");
+            break;
+    }
+    
+    return 0;
+}
