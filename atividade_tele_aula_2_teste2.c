62e4351f86a20422c72b49afb463ed9410e620a3
AndersonTeala <andersonteala@gmail.com>
2020-04-29 20:45:26 -0300

    Realizado atividade teleaula 2 exemplo 2

---
+++ atividade_tele_aula_2_teste2.c
@@ -0,0 +1,25 @@
+int main(){
+    int n;
+
+    do{
+        printf("\n Digite o valor de n \n");
+        scanf("%f", &n);
+
+    }while((n<=1,3,5,7,9) || (n>=2,4,6,8,10));
+
+    switch(n){
+        case 1:
+            if(n = 2,4,6,8,10);
+                n = n/2;
+            printf("\n resultado de n é par: %2f\n", n);
+        break;
+
+        case 2:
+            if(n = 1,3,5,7,9);
+                n = n*3+1;
+            printf("\n resultado de n é par: %2f\n", n);
+        break;
+    }
+
+    return 0;
+}
