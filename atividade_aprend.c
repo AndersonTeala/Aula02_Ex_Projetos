9baa2c3e3dc0d8f260ed4204d1ac591406893d72
AndersonTeala <andersonteala@gmail.com>
2020-04-29 19:05:35 -0300

    Realizado Atividade Teleaula 2

---
+++ atividade_aprend.c
@@ -0,0 +1,18 @@
+int main() {
+    float n;
+
+    printf("Digite o valor de n");
+    scanf("%f", &n);
+
+    if(n = 2,4,6,8,10){
+        n = n/2;
+    }
+    else
+    if(n = 1,3,5,7,9){
+        n = n*3+1;
+    }
+
+    printf ("\n resultado de n é: %2.f\n", n);
+
+      return 0;
+      }
